c317b0e212f3d2a3984fd688de1a5964d3e9fbd3
William Brittian <williambrittian13@gmail.com>
2025-02-06 10:51:11 -0600

    small testing fixes

--- src/game.cpp
+++ src/game.cpp
@@ -9,164 +9,158 @@
 
 #include "game.h"
 
-
 //
 // general game state functions
 //
 
-void Game::initializeGame(Player* player, int numBots, int startingCash) {
-    this->Deck.refillCards();
+void Game::initializeGame(Player *player, int numBots, int startingCash) {
+  this->Deck.refillCards();
 
-    this->User = player;
-    this->addPlayer(player, nullptr);
-    player->setNextPlayer(player);
+  this->User = player;
+  this->addPlayer(player, nullptr);
+  player->setNextPlayer(player);
 
-    // add bots
-    for (int i = 0; i < numBots; i++) {
-        string name = "Bot " + to_string(numBots - i);
+  // add bots
+  for (int i = 0; i < numBots; i++) {
+    string name = "Bot " + to_string(numBots - i);
 
-        this->addPlayer(new Bot(name, startingCash), player);
-    }
-
+    this->addPlayer(new Bot(name, startingCash), player);
+  }
 }
 
 void Game::finishGame() {
-    
-    while (this->NumPlayers > 0) {
-        removePlayer(this->FirstPlayer);
-    }
 
+  while (this->NumPlayers > 0) {
+    removePlayer(this->FirstPlayer);
+  }
 }
 
 void Game::newRound() {
-    this->Deck.refillCards();
+  this->Deck.refillCards();
 
-    // TODO: implement main game loop
+  // TODO: implement main game loop
 }
 
 void Game::settleRound() {
-    // TODO: implement code for determining winner
+  // TODO: implement code for determining winner
 
-    this->Bet = 0;
-    this->Pot = 0;
+  this->Bet = 0;
+  this->Pot = 0;
 }
 
-void Game::addPlayer(Player* player, Player* position) {
-    if (position == nullptr) {
-        this->FirstPlayer = player;
-    } else {
-        Player* temp = position->getNextPlayer();
-        position->setNextPlayer(player);
-        player->setNextPlayer(temp);
-    }
+void Game::addPlayer(Player *player, Player *position) {
+  if (position == nullptr) {
+    this->FirstPlayer = player;
+  } else {
+    Player *temp = position->getNextPlayer();
+    position->setNextPlayer(player);
+    player->setNextPlayer(temp);
+  }
 
-    this->NumPlayers++;
+  this->NumPlayers++;
 }
 
-void Game::removePlayer(Player* player) {
+void Game::removePlayer(Player *player) {
 
-    // TODO: what happens to user (and game) if you
-    // remove the player? When will this get called
-    // and will that happen?
+  // TODO: what happens to user (and game) if you
+  // remove the player? When will this get called
+  // and will that happen?
 
-    if (player->getNextPlayer() != player) {
-        if (this->FirstPlayer == player) {
-            this->FirstPlayer = player->getNextPlayer();
-        }
-
-        Player* previous = this->getPreviousPlayer(player);
-        Player* next = player->getNextPlayer();
-
-        previous->setNextPlayer(next);
-    } else {
-        this->FirstPlayer = nullptr;
-        this->User = nullptr;
+  if (player->getNextPlayer() != player) {
+    if (this->FirstPlayer == player) {
+      this->FirstPlayer = player->getNextPlayer();
     }
-    
-    this->NumPlayers--;
-    delete player;
+
+    Player *previous = this->getPreviousPlayer(player);
+    Player *next = player->getNextPlayer();
+
+    previous->setNextPlayer(next);
+  } else {
+    this->FirstPlayer = nullptr;
+    this->User = nullptr;
+  }
+
+  this->NumPlayers--;
+  delete player;
 }
 
 // rotates the head pointer to the next in the order
 // head by default points to the small blind
 void Game::rotateOrder() {
-    this->FirstPlayer = this->FirstPlayer->getNextPlayer();
+  this->FirstPlayer = this->FirstPlayer->getNextPlayer();
 }
-
 
 //
 // round active functions
 //
 
-void Game::settlePlayerBet(int amount, Player* player) {
-    if (this->Bet + amount < 0 ||
-        player->getCash() - amount < 0) {
-        throw range_error("Negative value error");
-    }
+void Game::settlePlayerBet(int amount, Player *player) {
+  if (this->Bet + amount < 0 || player->getCash() - amount < 0) {
+    throw range_error("Negative value error");
+  }
 
-    player->editCash(-amount);
-    this->Bet += amount;
+  player->editCash(-amount);
+  this->Bet += amount;
 }
 
-void Game::settlePlayerPot(int amount, Player* player) {
-    if (this->Pot + amount < 0 || 
-        player->getCash() - amount < 0) {
-        throw range_error("Negative value error");
-    }
+void Game::settlePlayerPot(int amount, Player *player) {
+  if (this->Pot + amount < 0 || player->getCash() - amount < 0) {
+    throw range_error("Negative value error");
+  }
 
-    player->editCash(-amount);
-    this->Pot += amount;
+  player->editCash(-amount);
+  this->Pot += amount;
 }
 
 void Game::settleBetPot(int amount) {
-    this->Bet -= amount;
-    this->Pot += amount;
+  this->Bet -= amount;
+  this->Pot += amount;
 }
 
-void Game::dealToPlayer(Player* player) {
-    player->emptyHand();
-    player->addCards(this->Deck.drawCards(2));
+void Game::dealToPlayer(Player *player) {
+  player->emptyHand();
+  player->addCards(this->Deck.drawCards(2));
 }
-
 
 //
 // misc
 //
-Player* Game::getNthPlayer(int N) {
-
-}
+Player *Game::getNthPlayer(int N) {}
 
 // returns the player behind the given player in the order
-Player* Game::getPreviousPlayer(Player* player) {
-    Player* current = player;
+Player *Game::getPreviousPlayer(Player *player) {
+  Player *current = player;
 
-    while (current->getNextPlayer() != player) {
-        current = current->getNextPlayer();
-    }
+  while (current->getNextPlayer() != player) {
+    current = current->getNextPlayer();
+  }
 
-    return current;
+  return current;
 }
-
 
 //
 // accessors
 //
-int Game::getPot() {
-    return this->Pot;
-}
+int Game::getPot() { return this->Pot; }
 
-int Game::getBet() {
-    return this->Bet;
-}
+int Game::getBet() { return this->Bet; }
 
-int Game::getNumPlayers() {
-    return this->NumPlayers;
-}
+int Game::getNumPlayers() { return this->NumPlayers; }
 
-Player* Game::getFirstPlayer() {
-    return this->FirstPlayer;
-}
+Player *Game::getFirstPlayer() { return this->FirstPlayer; }
 
-Player* Game::getCurrentLeader() {
-    
+// returns the player with the most cash
+Player *Game::getCurrentLeader() {
+  Player *leader = this->FirstPlayer;
+  Player *current = this->FirstPlayer->getNextPlayer();
+
+  while (current != this->FirstPlayer) {
+    if (current->getCash() > leader->getCash()) {
+      leader = current;
+    }
+
+    current = current->getNextPlayer();
+  }
+
+  return leader;
 }
--- test/unit/test_game_unit.cpp
+++ test/unit/test_game_unit.cpp
@@ -11,87 +11,92 @@
 
 #include "../fixtures.cpp"
 
-TEST_F(GameTest, Basic) {
-    EXPECT_EQ(2, 2);
-}
+TEST_F(GameTest, Basic) { EXPECT_EQ(2, 2); }
 
 TEST_F(GameTest, Initialize) {
-    game1.initializeGame(player1, 5, 1000);
+  game1.initializeGame(player1, 5, 1000);
 
-    EXPECT_EQ(game1.getNumPlayers(), 6);
+  EXPECT_EQ(game1.getNumPlayers(), 6);
 }
 
 TEST_F(GameTest, Betting) {
-    game2.settlePlayerPot(300, player1);
-    game2.settlePlayerPot(-100, player1);
+  game2.settlePlayerPot(300, player1);
+  game2.settlePlayerPot(-100, player1);
 
-    EXPECT_EQ(game2.getPot(), 200);
-    EXPECT_EQ(player1->getCash(), 800);
+  EXPECT_EQ(game2.getPot(), 200);
+  EXPECT_EQ(player1->getCash(), 800);
 
-    game2.settlePlayerBet(300, player1);
-    game2.settlePlayerBet(-100, player1);
+  game2.settlePlayerBet(300, player1);
+  game2.settlePlayerBet(-100, player1);
 
-    EXPECT_EQ(game2.getBet(), 200);
-    EXPECT_EQ(player1->getCash(), 600);
+  EXPECT_EQ(game2.getBet(), 200);
+  EXPECT_EQ(player1->getCash(), 600);
 
-    game2.settleBetPot(100);
-    game2.settleBetPot(-50);
+  game2.settleBetPot(100);
+  game2.settleBetPot(-50);
 
-    EXPECT_EQ(game2.getBet(), 150);
-    EXPECT_EQ(game2.getPot(), 250);
+  EXPECT_EQ(game2.getBet(), 150);
+  EXPECT_EQ(game2.getPot(), 250);
 }
 
 TEST_F(GameTest, getPreviousPlayer) {
-    Player* bot = game2.getPreviousPlayer(player1);
+  Player *bot = game2.getPreviousPlayer(player1);
 
-    EXPECT_EQ(bot->getName(), "Bot 3");
+  EXPECT_EQ(bot->getName(), "Bot 3");
 }
 
 TEST_F(GameTest, dealToPlayer) {
-    game2.dealToPlayer(player1);
+  game2.dealToPlayer(player1);
 
-    EXPECT_EQ(size(player1->getCards()), 2);
+  EXPECT_EQ(size(player1->getCards()), 2);
 }
 
 TEST_F(GameTest, rotateOrder) {
-    Player* nextPlayer = player1->getNextPlayer();
-    game2.rotateOrder();
-    
-    EXPECT_EQ(game2.getFirstPlayer(), nextPlayer);
+  Player *nextPlayer = player1->getNextPlayer();
+  game2.rotateOrder();
+
+  EXPECT_EQ(game2.getFirstPlayer(), nextPlayer);
 }
 
 TEST_F(GameTest, addPlayer_1) {
-    Player* nextPlayer = player1->getNextPlayer();
-    game2.addPlayer(player2, player1);
+  Player *nextPlayer = player1->getNextPlayer();
+  game2.addPlayer(player2, player1);
 
-    EXPECT_EQ(player1->getNextPlayer(), player2);
-    EXPECT_EQ(player2->getNextPlayer(), nextPlayer);
-    EXPECT_EQ(game2.getNumPlayers(), 5);
+  EXPECT_EQ(player1->getNextPlayer(), player2);
+  EXPECT_EQ(player2->getNextPlayer(), nextPlayer);
+  EXPECT_EQ(game2.getNumPlayers(), 5);
 }
 
 TEST_F(GameTest, addPlayer_2) {
-    Player* nextPlayer = player1->getNextPlayer();
-    Player* nextNextPlayer = nextPlayer->getNextPlayer();
-    game2.addPlayer(player2, nextPlayer);
+  Player *nextPlayer = player1->getNextPlayer();
+  Player *nextNextPlayer = nextPlayer->getNextPlayer();
+  game2.addPlayer(player2, nextPlayer);
 
-    EXPECT_EQ(nextPlayer->getNextPlayer(), player2);
-    EXPECT_EQ(player2->getNextPlayer(), nextNextPlayer);
-    EXPECT_EQ(game2.getNumPlayers(), 5);
+  EXPECT_EQ(nextPlayer->getNextPlayer(), player2);
+  EXPECT_EQ(player2->getNextPlayer(), nextNextPlayer);
+  EXPECT_EQ(game2.getNumPlayers(), 5);
 }
 
 TEST_F(GameTest, removePlayer_1) {
-    Player* nextPlayer = player1->getNextPlayer();
-    game2.removePlayer(player1);
+  Player *nextPlayer = player1->getNextPlayer();
+  game2.removePlayer(player1);
 
-    EXPECT_EQ(game2.getFirstPlayer(), nextPlayer);
-    EXPECT_EQ(game2.getNumPlayers(), 3);
+  EXPECT_EQ(game2.getFirstPlayer(), nextPlayer);
+  EXPECT_EQ(game2.getNumPlayers(), 3);
 }
 
 TEST_F(GameTest, removePlayer_2) {
-    Player* nextPlayer = player1->getNextPlayer();
-    Player* nextNextPlayer = nextPlayer->getNextPlayer();
-    game2.removePlayer(nextPlayer);
+  Player *nextPlayer = player1->getNextPlayer();
+  Player *nextNextPlayer = nextPlayer->getNextPlayer();
+  game2.removePlayer(nextPlayer);
 
-    EXPECT_EQ(player1->getNextPlayer(), nextNextPlayer);
-    EXPECT_EQ(game2.getNumPlayers(), 3);
+  EXPECT_EQ(player1->getNextPlayer(), nextNextPlayer);
+  EXPECT_EQ(game2.getNumPlayers(), 3);
+}
+
+TEST_F(GameTest, getCurrentLeader) {
+  game2.settlePlayerPot(200, player1->getNextPlayer());
+  game2.settlePlayerPot(-200, player1);
+
+  EXPECT_EQ(game2.getCurrentLeader(), player1);
 }
