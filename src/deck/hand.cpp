0e044fde36ecd1acc9c742383696ef983fc1b378
William Brittian <williambrittian13@gmail.com>
2025-03-12 11:48:42 -0500

    started implementing evaluate hand

--- src/deck/hand.cpp
+++ src/deck/hand.cpp
@@ -7,11 +7,26 @@
 // 2025
 //
 
-#include <map>
-
 #include "hand.hpp"
 
 void Hand::evaluateHand() {
-    std::map<int, int> ranks;
+    std::map<Rank, int> ranks;
     std::map<Suit, int> suits;
+
+    for (Card card : Cards) {
+        if(ranks[card.Rank] >= 3 && Type <= FOUR_OF_A_KIND) {
+            Type = FOUR_OF_A_KIND;
+        } else if (ranks[card.Rank] >= 2 && Type <- THREE_OF_A_KIND) {
+            Type = THREE_OF_A_KIND;
+        } else if (ranks[card.Rank] >= 1 && Type <= PAIR) {
+            Type = PAIR;
+        }
+
+        if(suits[card.Suit] >= 4 && Type <= FLUSH) {
+            
+        }
+
+        ranks[card.Rank]++;
+        suits[card.Suit]++;
+    }
 }
--- src/deck/hand.hpp
+++ src/deck/hand.hpp
@@ -9,6 +9,7 @@
 
 #pragma once
 
+#include <map>
 #include <vector>
 
 #include "card.hpp"
