21ae63e9badab986ec8de4393aba8991856d6b05
William Brittian <williambrittian13@gmail.com>
2025-10-21 22:33:05 -0500

    restructuring

--- src/core/game.hpp
+++ src/core/game.hpp
@@ -12,3 +12,47 @@
 #include <algorithm>
 #include <vector>
 
+enum Stage {
+    INACTIVE = 0,
+    PREFLOP,
+    FLOP,
+    TURN,
+    RIVER,
+    SHOWDOWN,
+    EVAL
+};
+
+class Game {
+  private:
+    bool Quit = false;
+
+    //
+    // general info
+    //
+    std::vector<Player> players;
+    std::vector<Player> playing;
+
+    int SmallBlind = 0;
+
+    Deck Deck;
+
+    int Round = 0;
+    bool Active = false;
+
+    //
+    // round-specific
+    //
+
+    Stage stage = INACTIVE;
+
+    int Pot = 0;
+    int Bet = 0;
+
+    std::vector<Card> Cards;
+
+  public:
+    //
+    // general game state functions
+    //
+    
+};
