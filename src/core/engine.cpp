47d237d93cf062c365eb664b9e1e80c7992b4471
William Brittian <williambrittian13@gmail.com>
2026-03-13 22:30:43 -0500

    fix engine helper function bugs

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -44,8 +44,9 @@ void PokerEngine::advanceEngine() {
 }
 
 void PokerEngine::beginRound() {
-    for (Seat& player : Players) {
-        player.Bet = 0;
+    for (int i = 0; i < Players.size(); i++) {
+        Players[i].Bet = 0;
+        Hands[i].Cards = TheDeck.drawCards(2);
     }
 
     Round += 1;
@@ -104,7 +105,7 @@ void PokerEngine::resolveShowdown() {
 
     // pay out winner(s)
     // TODO: add sidepot management
-    float split = ((float) Pot) / Playing;
+    float split = ((float) Pot) / bestHands.size();
 
     for (int i = 0; i < bestHands.size(); i++) {
         int payout;
@@ -121,7 +122,16 @@ void PokerEngine::resolveShowdown() {
 }
 
 void PokerEngine::resolveFold() {
+    resolveBetting();
 
+    for (Seat& player : Players) {
+        if (player.Active) {
+            player.Cash += Pot;
+            break;
+        }
+    }
+
+    resetRound();
 }
 
 void PokerEngine::resetRound() {
@@ -173,7 +183,7 @@ int PokerEngine::getLast(const int& raiser) {
     int current = raiser;
     Seat currentPlayer;
     do {
-        current = getPlayer(current, 1);
+        current = getPlayer(current, -1);
         currentPlayer = Players[current];
     } while (!currentPlayer.Active);
     return current;
@@ -198,34 +208,37 @@ void PokerEngine::resolveBet(const int& pid, const int& amount) {
 void PokerEngine::addBet(const int& pid, const int& amount) {
     Seat& player = getSeat(pid);
 
-    player.Cash -= amount;
+    int diff = amount - player.Bet;
+    player.Cash -= diff;
     player.Bet = amount;
 }
 
-// returns -1 if h1 is better, 0 if tie, 1 if h2
+// returns -1 if h1 is better, 0 if tie, 1 if h2 is better
 int PokerEngine::compareHands(const Hand& h1, const Hand& h2) {
     if (h1.Type > h2.Type) {
         return -1;
     } else if (h1.Type < h2.Type) {
         return 1;
     } else {
-        
+        if (h1.HandHigh > h2.HandHigh) return -1;
+        if (h1.HandHigh < h2.HandHigh) return 1;
+        if (h1.OtherHigh > h2.OtherHigh) return -1;
+        if (h1.OtherHigh < h2.OtherHigh) return 1;
+        return 0;
     }
 }
 
 Seat& PokerEngine::getSeat(const int& pid) {
-    // quick jailbreak in case pid = seat number in vector
     int numPlayers = Players.size();
     if (pid < numPlayers && Players[pid].PlayerId == pid) {
         return Players[pid];
-    } else {
-        for (int i = 0; i < numPlayers; i++) {
-            Seat player = Players[i];
-            if (player.PlayerId == pid) {
-                return player;
-            }
+    }
+    for (int i = 0; i < numPlayers; i++) {
+        if (Players[i].PlayerId == pid) {
+            return Players[i];
         }
     }
+    return Players[0];
 }
 
 int PokerEngine::getIdx(const int& pid) {
@@ -234,6 +247,7 @@ int PokerEngine::getIdx(const int& pid) {
             return i;
         }
     }
+    return -1;
 }
 
 //
@@ -300,11 +314,11 @@ bool PokerEngine::submitAction(const Action& action) {
 
         if (amount == 0) {
             bet = CurrentBet;
-        } else if (bet >= seat.Cash) {
+        } else if (amount >= seat.Cash) {
             bet = seat.Cash;
             LastAction = getLast(pid);
             AllIn = true;
-        } else if (bet < 2 * CurrentBet) {
+        } else if (amount < std::max(2 * CurrentBet, BigSize)) {
             return false;
         } else {
             bet = amount;
@@ -319,6 +333,7 @@ bool PokerEngine::submitAction(const Action& action) {
 
         if (Playing == 1) {
             resolveFold();
+            return true;
         }
     } else if (type == NONE) {
         return false;
